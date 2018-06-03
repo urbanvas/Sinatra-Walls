2e1d2ff6da610447845d1e282285a9a10077a6c5
vasu <vasu@vasus-MBP.fios-router.home>
2018-06-03 19:16:15 -0400

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,35 @@ class UsersController < ApplicationController
     if logged_in?
       redirect "/show"
     else
-      redirect "/login"
+      erb :"user/login"
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/show"
+    else
+      redirect "/"
     end
   end
 
   get "/show" do
-    "Show page"
+    if logged_in?
+      erb :"user/show"
+    else
+      redirect '/'
+    end
+  end
+
+  get "/logout" do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to "/login"
+    else
+      redirect to "/"
+    end
   end
 
 
